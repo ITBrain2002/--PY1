0502fe19ccd440a675e1bf8b663ad7f45c0b476a
ITBrain2002 <115505926+ITBrain2002@users.noreply.github.com>
2022-11-05 23:54:04 +0300

    Delete task_5.py

--- Лабораторная работа 5/task_5.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-def get_random_password(x= 8) -> str:
-
-    str= string.ascii_uppercase + string.ascii_lowercase+ string.digits  # TODO написать функцию генерации случайных паролей
-    str = random.sample(str,x)
-    return str
-
-print(get_random_password())
